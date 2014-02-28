d81a24b808ad7f5f24ed7a0d059c16abfb499b12
TheJKFever <Jon.Koehmstedt@gmail.com>
2014-02-28 02:18:11 -0800

    css to scss

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
 
 	def scoreboard
-		@tdScores = Highscore.joins(:game).where("games.name = ?", "Tower Defense")
-		@psScores = Highscore.joins(:game).where("games.name = ?", "Phaser Stars")
+		@tdScores = Highscore.joins(:game).where("games.name = ?", "Tower Defense").order("highscores.value DESC")
+		@psScores = Highscore.joins(:game).where("games.name = ?", "Phaser Stars").order("highscores.value DESC")
 	end
 
 	def towerDefense
-		@highscores = Highscore.joins(:game).where("games.name = ?", "Tower Defense")
+		@highscores = Highscore.joins(:game).where("games.name = ?", "Tower Defense").order("highscores.value DESC")
 	end
 
 	def phaserStars
-		@highscores = Highscore.joins(:game).where("games.name = ?", "Phaser Stars")
+		@highscores = Highscore.joins(:game).where("games.name = ?", "Phaser Stars").order("highscores.value DESC")
 	end
 end
