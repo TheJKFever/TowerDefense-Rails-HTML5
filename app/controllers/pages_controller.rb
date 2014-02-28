2bd4e8c56afdb8323c42289a8832a3c0f1ff5fb8
TheJKFever <Jon.Koehmstedt@gmail.com>
2014-02-28 01:35:50 -0800

    fixed controller db call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
 
 	def scoreboard
-		@tdScores = Highscore.joins(:game).where('games.name = "Tower Defense"')
-		@psScores = Highscore.joins(:game).where('games.name = "Phaser Stars"')
+		@tdScores = Highscore.joins(:game).where("games.name = ?", "Tower Defense")
+		@psScores = Highscore.joins(:game).where("games.name = ?", "Phaser Stars")
 	end
 
 	def towerDefense
-		@highscores = Highscore.joins(:game).where('games.name = "Tower Defense"')
+		@highscores = Highscore.joins(:game).where("games.name = ?", "Tower Defense")
 	end
 
 	def phaserStars
-		@highscores = Highscore.joins(:game).where(games.name = 'Phaser Stars')
+		@highscores = Highscore.joins(:game).where("games.name = ?", "Phaser Stars")
 	end
 end
