9e1b86cd788ae4865bd085dfb14ce4d50b9bb0d9
TheJKFever <Jon.Koehmstedt@gmail.com>
2014-02-28 02:33:05 -0800

    added phaser.js to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ TowerDefense::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( phaser.min.js )  # => ["phaser.min.js", ...]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
